86aff040f84708422b0f545af66155571eb27042
steden <1470804@qq.com>
2021-05-15 18:43:57 +0800

    添加PO

--- 04_Component（业务组件）/FOPS.Com.K8SServer/Cluster/Dal/ClusterPO.cs
+++ 04_Component（业务组件）/FOPS.Com.K8SServer/Cluster/Dal/ClusterPO.cs
@@ -1,7 +1,19 @@
+using FS.Core.Mapping.Attribute;
+using Nest;
+
 namespace FOPS.Com.K8sServerAA.Cluster.Dal
 {
     public class ClusterPO
     {
-        
+        /// <summary>
+        /// 集群名称
+        /// </summary>
+        [Field(Name = "name")]
+        public string Name { get; set; }
+        /// <summary>
+        /// 本地kubectl配置地址
+        /// </summary>
+        [Field(Name = "config_name")]
+        public string ConfigName { get; set; }
     }
 }
