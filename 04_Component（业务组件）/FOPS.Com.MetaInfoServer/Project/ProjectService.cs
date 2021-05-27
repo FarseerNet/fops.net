57b3cd3247322c156151c23e27d4da106046f5a2
steden <1470804@qq.com>
2021-05-28 01:54:15 +0800

    缺少字段显示

--- 04_Component（业务组件）/FOPS.Com.MetaInfoServer/Project/ProjectService.cs
+++ 04_Component（业务组件）/FOPS.Com.MetaInfoServer/Project/ProjectService.cs
@@ -41,7 +41,7 @@ namespace FOPS.Com.MetaInfoServer.Project
         /// </summary>
         public async Task<List<ProjectVO>> ToListAsync(int groupId)
         {
-            var lstPo = await MetaInfoContext.Data.Project.Where(o => o.GroupId == groupId).Select(o => new {o.Id, o.Name, o.DockerVer, o.ClusterVer, o.DockerHub}).ToListAsync();
+            var lstPo = await MetaInfoContext.Data.Project.Where(o => o.GroupId == groupId).Select(o => new {o.Id, o.Name, o.DockerVer, o.ClusterVer, o.DockerHub,o.GroupId}).ToListAsync();
             var lst   = new List<ProjectVO>();
             foreach (var po in lstPo)
             {
