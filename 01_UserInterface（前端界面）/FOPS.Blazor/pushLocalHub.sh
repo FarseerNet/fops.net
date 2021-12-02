4a2925a6d67f3e0d551a57b77ecbad6f7e9736c5
steden <1470804@qq.com>
2021-12-02 23:49:26 +0800

    修改新的配置结构

--- 01_UserInterface（前端界面）/FOPS.Blazor/pushLocalHub.sh
+++ 01_UserInterface（前端界面）/FOPS.Blazor/pushLocalHub.sh
@@ -7,8 +7,5 @@ cd bin/release/net5.0/publish
 docker build -t fops:latest --network=host .
 
 docker run -d --name fops -p 88:88 \
--e Database__Items__0__Server=docker.for.mac.host.internal \
--e FSS__Server__0__Url=http://docker.for.mac.host.internal:888 \
--e ElasticSearch__0__Server=http://docker.for.mac.host.internal:9200 \
---network=net fops:latest \
- --restart=always
+--network net --network-alias fops fops:latest \
+--restart=always
