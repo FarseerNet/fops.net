2d5f7064e4802ea17c9c70915e5f67bd48067c5a
steden <1470804@qq.com>
2021-12-18 00:13:47 +0800

    升级FSS、升级.NET 6

--- 03_Abstract（组件接口）/FOPS.Abstract/Fss/Entity/TaskGroupVO.cs
+++ 03_Abstract（组件接口）/FOPS.Abstract/Fss/Entity/TaskGroupVO.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace FOPS.Abstract.Fss.Entity
 {
@@ -12,65 +14,71 @@ namespace FOPS.Abstract.Fss.Entity
         /// 主键
         /// </summary>
         public int Id { get; set; }
-        
+
         /// <summary>
         /// 任务组标题
         /// </summary>
         public string Caption { get; set; }
-        
+
         /// <summary>
         /// 实现Job的特性名称（客户端识别哪个实现类）
         /// </summary>
         public string JobName { get; set; }
-        
+
         /// <summary>
         /// 传给客户端的参数，按逗号分隔
         /// </summary>
-        public string Data { get; set; }
-        
+        public Dictionary<string, string> Data { get; set; }
+
+        public string DataString
+        {
+            get => JsonConvert.SerializeObject(Data);
+            set => Data = JsonConvert.DeserializeObject<Dictionary<string, string>>(value);
+        }
+
         /// <summary>
         /// 开始时间
         /// </summary>
         public DateTime StartAt { get; set; }
-        
+
         /// <summary>
         /// 下次执行时间
         /// </summary>
         public DateTime NextAt { get; set; }
-        
+
         /// <summary>
         /// 时间间隔
         /// </summary>
         public long IntervalMs { get; set; }
-        
+
         /// <summary>
         /// 时间定时器表达式
         /// </summary>
         public string Cron { get; set; }
-        
+
         /// <summary>
         /// 活动时间
         /// </summary>
         public DateTime ActivateAt { get; set; }
-        
+
         /// <summary>
         /// 最后一次完成时间
         /// </summary>
         public DateTime LastRunAt { get; set; }
-        
-        /// <summary>
-        /// 运行平均耗时
-        /// </summary>
-        public long RunSpeedAvg { get; set; }
-        
-        /// <summary>
-        /// 运行次数
-        /// </summary>
-        public int RunCount { get; set; }
-        
+
         /// <summary>
         /// 是否开启
         /// </summary>
         public bool IsEnable { get; set; }
+
+        /// <summary>
+        /// 运行平均耗时
+        /// </summary>
+        public long RunSpeedAvg { get; set; }
+
+        /// <summary>
+        /// 运行次数
+        /// </summary>
+        public int RunCount { get; set; }
     }
 }
